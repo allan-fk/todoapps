4d6ba705a09626e43f076999026caba318c1bee3
klein <allan.klein@epitech.eu>
2022-06-19 06:19:42 +0200

    add comments

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
+  # root => redirection de la racine
   get "/articles", to: "articles#test"
+  # get "/articles" => route
+  # to: "articles#test" => test un des controller de articles
 end
